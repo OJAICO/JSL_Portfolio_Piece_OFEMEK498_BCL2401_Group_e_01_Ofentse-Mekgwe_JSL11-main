d2eb6bec8c51477b54490221d304ad30652b5739
OJAICO <ofentsemekgwe911@gmail.com>
2024-04-24 11:06:56 +0200

    Fixed an syntax error at line 55

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function fetchAndDisplayBoardsAndTasks() {
 // Creates different boards in the DOM
 // TASK: Fix Bugs
 function displayBoards(boards) {
-  const boardsContainer = document.querySelectord("#boards-nav-links-div");
+  const boardsContainer = document.querySelector("#boards-nav-links-div");
   boardsContainer.innerHTML = ''; // Clears the container
   boards.forEach(board => {
     const boardElement = document.createElement("button");
